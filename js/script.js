a0d452481f38b776db7844c264d67f5d28a225c0
Natalya Filatenkova <Filatenkova87@yandex.ru>
2021-06-13 19:52:56 +0300

    JavaScript 2.0 / Уроки 11-19

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Основы
+ */
 // Выберет элемент с id = out
 document.getElementById('out').innerHTML = 2021
 
@@ -28,7 +31,9 @@ button.onclick = function() {
     inputIn.value = '' // очищаем значение input
 }
 
-// Работаем с формами
+/**
+ * Работаем с формами
+ */
 // onclick срабатывает при клике на элемент
 document.querySelector('[data-btn-color]').onclick = function () {
     let color = document.querySelector('[data-input-color]').value
@@ -59,3 +64,90 @@ document.querySelector('[data-btn-textarea]').onclick = function (event) {
     // Найдем элемент формы с id = formText
     console.log(form.elements.formText.value)
 }
+
+/**
+ * Интерполяция/Конкатенация
+ */
+let out = document.querySelector('[data-out]')
+for (let i = 0; i < 10; i++) {
+    out.innerHTML += '3* ' + i + ' = ' + (i * 3) + '<br>'; // Конкатенация
+}
+out.innerHTML += '<hr>'
+
+for (let i = 0; i < 10; i++) {
+    out.innerHTML += `3 * ${i} = ${3 * i}` + '<br>'; // Интерполяция
+}
+out.innerHTML += '<hr>'
+
+/**
+ * textContent/innerHTML
+ */
+let paragraph = document.querySelector('[data-paragraph]')
+console.log(paragraph.innerHTML) // получим текст с html-тегами
+console.log(paragraph.innerText) // отформатированный текст, но без html-тегов. Введен для Internet Explorer
+console.log(paragraph.textContent) // получим текст без html-тегов. Работает быстрее. Защищает от XSS атак
+
+let wrapper = document.querySelector('[data-wrapper]')
+wrapper.textContent = paragraph.textContent
+
+/**
+ * Циклы While и Do while
+ */
+// Пока k < 5, цикл будет выполняться
+let k = 0;
+while (k < 5) {
+    k++;
+    console.log(k);
+}
+
+// Необходимо, что тело цикла выполнилось хотя бы один раз
+do {
+    console.log(k);
+    k++;
+} while (k < 3) {
+    console.log(k);
+}
+
+/**
+ * Работа с DOM, создание элементов, работа с CSS
+ */
+wrapper.style.width = '600px'
+//console.log(wrapper.style); // посмотреть все стили
+
+let btnArea = document.querySelector('[data-btn-textarea]')
+btnArea.onclick = function(e) {
+    e.preventDefault()
+    //this.classList.add('bgr')
+    //this.classList.remove('bgr')
+    this.classList.toggle('bgr')
+}
+
+// Атрибуты data
+let newParagraph = document.querySelector('[data-new-paragraph]')
+console.log(newParagraph.getAttribute('data'))
+
+let link = document.querySelector('[data-link]')
+console.log(link.getAttribute('href'))
+
+// Если на странице несколько элементов с атрибутом data-link, используем querySelectorAll
+let links = document.querySelectorAll('[data-link]')[1]
+console.log(links.getAttribute('href'))
+
+// Установим атрибут
+newParagraph.setAttribute('data-num', 6)
+
+// Работа с массивом
+let gas = document.querySelectorAll('[data-gas]')
+for (let i = 0; i < gas.length; i++) {
+    gas[i].onclick = function() {
+        let gallons = document.querySelector('[data-gallons]').value
+        let amount = this.getAttribute('data-value')
+        console.log(gallons * amount)
+    }
+}
+
+// Создать элемент
+let a = document.createElement('div')
+a.innerHTML = 'Hello!'
+a.classList.add('bgr')
+newParagraph.appendChild(a) // добавим элемент внутрь newParagraph
