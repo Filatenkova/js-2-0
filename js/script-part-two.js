840711fd86c0e5af741966bb10d51c59dc1bfe87
Natalya Filatenkova <Filatenkova87@yandex.ru>
2021-06-18 00:50:09 +0300

    JavaScript 2.0 / Уроки 41-44

--- js/script-part-two.js
+++ js/script-part-two.js
@@ -16,3 +16,61 @@ meAlert.showAlert();
 
 let upgradeAlert = new UpgradeAlert('New message', 'bgr-orange', '[data-text]', 'pets')
 upgradeAlert.showIconAlert();
+
+/**
+ * Рекурсия
+ * Функция вызывает сама себя
+ */
+
+let rec = 1;
+
+function recursion() {
+    rec++
+    console.log(rec)
+    // Условие для выхода
+    if (rec > 10) {
+        return rec
+    }
+    recursion()
+}
+
+// Пример применения рекурсии для анимации
+let offset = 0;
+function move() {
+    offset += 5
+    document.querySelector('[data-animation]').style.left = offset + 'px'
+    if (offset > 273) {
+        return true
+    }
+    // Сделаем задержку 500 миллисекунд
+    setTimeout(move, 500)
+}
+
+document.querySelector('[data-animation-btn]').onclick = move
+
+/**
+ * Замыкания
+ * «Замыкания» - это замыкание области видимости на какой-то объем (scope) переменной
+ */
+// Глобальная переменная по отношению к функции
+// Проблема 1 - переменная не защищена и ее возможно переопределить в любом месте кода, поэтому нам нежелательно вызывать ее в функции
+let lock = 0
+function lockingFalse() {
+    // Локальная переменная, видна только внутри функции. Правильнее использовать локальную переменную из-за проблемы 1
+    // Проблема 2 - как сделать счетчик, если каждый вызов переменная обнуляется до дефолтного значения
+    lock = 2
+    lock = lock + 1
+    return lock
+}
+console.log(lockingFalse())
+
+// Решение проблемы 2
+function lockingTrue() {
+    // Чтобы переменная была доступна только внутри, замыкаем ее в области видимости. Аналог private из ООП
+    lock = 2
+    return function() {
+        lock = lock + 1
+        return lock
+    }
+}
+console.log(lockingTrue())
